dcdbcd8f83bc9a82d2315e16985af0455f1da8b3
Demaceo Howard <hdemaceo@gmail.com>
2020-12-16 10:08:21 -0700

    Construct Reservation component

---
+++ src/Components/Reservation/Reservation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Reservation.css'
+export default function Reservation(props) {
+    return (
+      <section className="reservation">
+        <h3>{props.name}</h3>
+        <p>{props.date}</p>
+        <p>{props.time}</p>
+        <p>Number of Guests: {props.number}</p>
+        <button onClick={() => props.cancelRes(props.id)}>Cancel</button>
+      </section>
+    );
+}
