ee14736366406241519c4afd0331fc81f7f507eb
Demaceo Howard <hdemaceo@gmail.com>
2020-12-16 10:07:04 -0700

    Fetch api data

---
+++ src/apiCalls.js
@@ -0,0 +1,5 @@
+export const getAllReservations = () => {
+  return fetch("http://localhost:3001/api/v1/reservations")
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+};
